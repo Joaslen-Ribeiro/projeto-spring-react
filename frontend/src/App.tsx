bf5c851f946449538a858908304310e963e72b97
Joaslen-Ribeiro <joaslen@hotmail.com>
2021-11-01 16:55:20 -0300

    components basicos estaticos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
+import NavBar from "components/NavBar";
+import Footer from "components/NavBar/Footer";
+import DataTable from "components/NavBar/DataTable";
 
 function App() {
   return (
-    <div>
+    <>
+    <NavBar/>
+    <div className="container">
       <h1 className="text-primary">Olá mundo!</h1>
+      <DataTable/>
     </div>
+    <Footer/>
+    </>
   );
 }
 

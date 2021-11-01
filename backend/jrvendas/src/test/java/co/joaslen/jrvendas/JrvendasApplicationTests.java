15f402cc55a61c5e32ec5551d29029c826d25c6b
Joaslen-Ribeiro <joaslen@hotmail.com>
2021-11-01 15:15:35 -0300

    criação do front e back

---
+++ backend/jrvendas/src/test/java/co/joaslen/jrvendas/JrvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package co.joaslen.jrvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JrvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
